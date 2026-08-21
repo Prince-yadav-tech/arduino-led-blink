0737255a3720efe073a10f3356b21a338d713c3d
Prince-yadav-tech <prince.yadav.0117@gmail.com>
2026-08-21 16:14:56 +0530

    Update led_blink.ino

--- src/led_blink.ino
+++ src/led_blink.ino
@@ -1,1 +1,18 @@
+// Pin definitions
+const int onboardLED = 13;
+const int externalLED = 8;
 
+void setup() {
+  pinMode(onboardLED, OUTPUT);
+  pinMode(externalLED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(onboardLED, HIGH);
+  digitalWrite(externalLED, HIGH);
+  delay(1000);
+
+  digitalWrite(onboardLED, LOW);
+  digitalWrite(externalLED, LOW);
+  delay(1000);
+}
